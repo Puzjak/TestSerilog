ec2e4cfbc798761a20d0699a73fdeb57ee42f279
Matija Puzjak <puzjakmatija@gmail.com>
2022-03-17 18:08:21 +0100

    Initial commit

---
+++ TestSerilog/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+using TestSerilog.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITestService, TestService>();
+
+var serilogConfig = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration);
+
+builder.Services.AddLogging(config => config.AddSerilog(serilogConfig.CreateLogger()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ TestSerilog/Services/TestService.cs
@@ -0,0 +1,28 @@
+﻿namespace TestSerilog.Services
+{
+    public interface ITestService
+    {
+        void TestMethod();
+
+        void TestException();
+    }
+    public class TestService : ITestService
+    {
+        private readonly ILogger<TestService> _logger;
+
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void TestException()
+        {
+            throw new NotImplementedException("Test exception method is not implemented");
+        }
+
+        public void TestMethod()
+        {
+            _logger.LogInformation("This is a test method.");
+        }
+    }
+}
